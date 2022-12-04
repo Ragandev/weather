10cc680b29fb313948dd865742d00f5023a73280
Ranjith <117666909+Ragandev@users.noreply.github.com>
2022-12-04 15:45:18 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const http = require("http");
+const { response } = require("express");
+
+const app = express();
+
+// app.use(http);
+app.use(bodyParser.urlencoded({extended:true}));
+
+
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function(req, res){
+    var cityName = req.body.city;
+    var userUnit = req.body.unit;
+
+    var apiKey = "a981b9987dd26957edeccddf5a25df3e";
+    var url = "http://api.weatherstack.com/current?access_key="+apiKey+"&query="+cityName+"&units="+ userUnit;
+
+    http.get(url, function(response){
+        
+        response.on("data", function(data){
+            var weather = JSON.parse(data);
+
+            var temp =  weather.current.temperature;
+            var dis = weather.current.weather_descriptions[0];
+
+        res.write("<h1>"+ cityName +"</h1>");
+        res.write("<h3>Temperature is "+ temp +"</h3>");
+        res.write("<h3>Weather is "+ dis +"</h3>");
+        res.send();
+
+        });
+
+        
+
+    });
+
+    // console.log(cityName);
+    // console.log(userUnit);
+});
+app.listen(3000);
+console.log("Server Started");
